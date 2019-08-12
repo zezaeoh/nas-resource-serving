f73eae649abf1c2c731d354e46a87e2b9edd1a7d
오재건 <jaegeon@dano.me>
2019-08-12 10:11:58 +0900

    Initial commit

---
+++ src/main/java/com/zezaeoh/nasresourceserving/NasResourceServingApplication.java
@@ -0,0 +1,13 @@
+package com.zezaeoh.nasresourceserving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasResourceServingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NasResourceServingApplication.class, args);
+    }
+
+}
